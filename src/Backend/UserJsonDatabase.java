33ae076feefd5b50baa47e04538fc5010bd2c8f6
HazemMagdyIsmail <hazemmagdyism@gmail.com>
2025-11-16 23:15:32 +0200

    Update UserJsonDatabase.java

--- src/Backend/UserJsonDatabase.java
+++ src/Backend/UserJsonDatabase.java
@@ -288,7 +288,7 @@ public boolean validateLoginStudent(String username, String password) {
     // Check students
     for (Student s : students) {
         if (s.getUsername().equals(username) && s.getPasswordHash().equals(password)) {
-            return true;
+            return true  ;
         }
     }
 
